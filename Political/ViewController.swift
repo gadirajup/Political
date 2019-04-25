8117211843820c084d69acd4f25d9c1f1a045b56
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-24 18:27:39 -0700

    Initial Commit

---
+++ Political/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Political
+//
+//  Created by Prudhvi Gadiraju on 4/24/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
